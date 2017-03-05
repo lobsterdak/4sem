f7180db3cbd8f1e780548e51b88a8e97d1c9fa2f
Vlad <vlad.miryaha@gmail.com>
2017-03-05 03:06:10 +0300

    ok
    read comments

--- Vector2/Vector2.h
+++ Vector2/Vector2.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <iostream>
+
+/*
+    Зачем этот define нужен?
+*/
 #define _USE_MATH_DEFINES
 #include <math.h>
 
@@ -8,10 +12,40 @@ class Vector2
 {
     public:
         double x, y;
+    
+        /*
+            Уберите реализацию в cpp файл.
+            
+            Можно написать так, кстати:
+            Vector2(double x, double y): x(x), y(y)
+            {}
+            
+            Либо
+            
+            Vector2(double x, double y):
+            {
+                this->x = x;
+                this->y = y;
+            }
+            
+            Мы просто ещё не успели на семинаре обсудить, что такое this,
+            и что такое списки инициализации.
+        */
         Vector2(double a, double b):
         x (a), y (b)
         {}
+    
+        /*
+            fixit: нужен ещё пустой конструктор.
+            Если бы вы не написали никакого контструктора, то пустой сгенерился бы автоматически.
+            Если вы написали сами хоть какой-то, то пустой автоматически не сгенерируется.
+        */
+    
+        /*
+            Можно было бы деструктор не писать, и он бы сгенерировался таким сам.
+        */
         ~Vector2(){}
+    
         Vector2& operator+=(const Vector2& other);
         Vector2& operator-=(const Vector2& other);
         Vector2 operator-() const;
