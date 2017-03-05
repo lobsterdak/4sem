48519579dce889751051bb51826af36a74d0167d
Vlad <vlad.miryaha@gmail.com>
2017-03-05 03:14:38 +0300

    read comments

--- Vector2/Vector2.cpp
+++ Vector2/Vector2.cpp
@@ -15,6 +15,12 @@ std::istream& operator >> (std::istream & stream, Vector2 & v)
 
 Vector2 operator*(double c, const Vector2& v)
 {
+    /*
+    переиспользовать уже написанный код - хорошо.
+    
+    можно было бы написать просто
+    return v * c;
+    */
     return Vector2(c * v.x, c * v.y);
 }
 
@@ -35,12 +41,16 @@ Vector2 operator-(const Vector2& first, const Vector2& second)
 
 double Vector2::len() const
 {
+        /*
+            внешние скобки можно опустить
+        */
         return (sqrt(x * x + y * y));
 }
 
 Vector2 Vector2::unit_vector() const
 {
-    return Vector2(x / len(), y / len());
+    // с точки зрения производительности вы дважды вызвали "долгую" ф-ю вычисления корня ...
+    return Vector2(x / len(), y / len()); 
 }
 
 Vector2 Vector2::normal() const
@@ -84,12 +94,23 @@ Vector2 operator/(const Vector2& v, double c)
 
 Vector2 Vector2::rotate(double degree)
 {
+    /*
+      fixit
+      1) по логике rotate должен изменять свои x и y ... и возвращать Vector2
+      2) getRotated - не должен изменять "себя", т.е. должен быть константный
+    */
     double rad = degree / 180 * M_PI;
     return Vector2(x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad));
 }
 
 Vector2& Vector2::getRotate(double degree)
 {
+    /*
+      fixit
+      вы дважды написали код математики поворота.
+      после того, как поправите rotate, то getRotated пишется в одну строчку:
+      return Vector2(x, y).rotate(angle);
+    */
     double rad = degree / 180 * M_PI;
     double xtmp = x;
     x = x * cos(rad) - y * sin(rad);
